28b184aa8e3d17d8f28c90acc32e1d081547d7d9
aapaev <aapaev@zuzex.com>
2024-07-26 16:04:09 +0300

    fix: writed types for myApp component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { FC } from 'react';
 import 'tailwindcss/base.css';
 import '../styles/index.css';
+import { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
