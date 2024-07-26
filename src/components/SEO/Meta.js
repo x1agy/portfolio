e40b7dcf36532c4921c449de3f5f22a1783a526e
aapaev <aapaev@zuzex.com>
2024-07-26 16:27:54 +0300

    feature: changed meta data and some links to myself ones

--- src/components/SEO/Meta.js
+++ src/components/SEO/Meta.js
@@ -1,71 +1,59 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 
 export default function Meta() {
   return (
     <Head>
       /* Primary Meta Tags */
-      <title>Aiden Mori Portfolio - Web & Blockchain Engineer</title>
+      <title>Апаев Ахмед - Frontend developer - Портфолио</title>
       <meta charSet="utf-8" />
-      <meta
-        name="title"
-        content="Aiden Mori Portfolio - Web & Blockchain Engineer"
-      />
+      <meta name="title" content="Апаев Ахмед - Frontend developer - Портфолио" />
       <meta
         name="description"
-        content="Aiden Mori's (aiden77mori) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Персональное портфолио фронтенд разработчика Апаева Ахмеда, x1agy"
       />
       <meta name="author" content="Aiden Mori (aiden77mori)" />
       <meta
         name="keywords"
-        content="aiden77mori, aiden77mori's portfolio, blockchain, engineer, web, smart contract engineer, smart contract, solidity, react, web3.js, aiden77mori linux, ubuntu portfolio, aiden mori protfolio, aiden mori computer, aiden mori, aiden ubuntu, aiden mori ubuntu portfolio"
+        content="x1agy, skizzzgrand, xhhdhe, Апаев, апаев, Апаев Ахмед, апаев ахмед, ахмед апаев, Ахмед Апаев, Ахмед апаев, Апаев ахмед, фронт, фронт разраб, фронтенд разработчик, разработчик, фронтенд, front, front dev, frontend, frontend developed, front dev"
       />
       <meta name="robots" content="index, follow" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="language" content="English" />
+      <meta name="language" content="Russian" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#E95420" />
       /* Search Engine */
       <meta name="image" content="images/logos/fevicon.png" />
       /* Schema.org for Google */
-      <meta
-        itemProp="name"
-        content="Aiden Mori Portfolio - Web & Blockchain Engineer"
-      />
+      <meta itemProp="name" content="Апаев Ахмед - Frontend developer - Портфолио" />
       <meta
         itemProp="description"
-        content="Aiden Mori's (aiden77mori) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Персональное портфолио фронтенд разработчика Апаева Ахмеда, x1agy"
       />
-      <meta itemProp="image" content="images/logos/aidenmori.png" />
+      <meta itemProp="image" content="images/logos/ubuntu-logo.png" />
       /* Twitter */
       <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:title"
-        content="Aiden Mori Portfolio - Web & Blockchain Engineer"
-      />
+      <meta name="twitter:title" content="Апаев Ахмед - Frontend developer - Портфолио" />
       <meta
         name="twitter:description"
-        content="Aiden Mori's (aiden77mori) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Персональное портфолио фронтенд разработчика Апаева Ахмеда, x1agy"
       />
       <meta name="twitter:site" content="aiden77mori" />
       <meta name="twitter:creator" content="aiden77mori" />
-      <meta name="twitter:image:src" content="images/logos/aidenmori.png" />
+      <meta name="twitter:image:src" content="images/logos/ubuntu-logo.png" />
       /* Open Graph general (Facebook, Pinterest & Google+) */
-      <meta
-        name="og:title"
-        content="Aiden Mori Portfolio - Web & Blockchain Engineer"
-      />
+      <meta name="og:title" content="Апаев Ахмед - Frontend developer - Портфолио" />
       <meta
         name="og:description"
-        content="Aiden Mori's (aiden77mori) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Персональное портфолио фронтенд разработчика Апаева Ахмеда, x1agy"
       />
-      <meta name="og:image" content="images/logos/aidenmori.png" />
+      <meta name="og:image" content="images/logos/ubuntu-logo.png" />
       <meta name="og:url" content="http://aiden77mori.github.io/" />
-      <meta name="og:site_name" content="Aiden Mori Personal Portfolio" />
+      <meta name="og:site_name" content="Портфолио Апаева Ахмеда" />
       <meta name="og:locale" content="en_IN" />
       <meta name="og:type" content="website" />
-      <link rel="icon" href="images/logos/aidenmori.png" />
-      <link rel="apple-touch-icon" href="images/logos/aidenmori.png" />
+      <link rel="icon" href="images/logos/ubuntu-logo.png" />
+      <link rel="apple-touch-icon" href="images/logos/ubuntu-logo.png" />
       <link
         rel="preload"
         href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
